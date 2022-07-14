5f4edef7d66c593cfa31cd31c28d15394b195eda
javulm <javulmulla8@gmail.com>
2022-07-14 17:03:18 +0530

    [javul]unit conversion pgm

---
+++ conversionpgm.sh
@@ -0,0 +1,16 @@
+
+read -p "enter the number :" N
+
+inch=12
+feet=$((N/inch))
+echo $feet"ft"
+mtr=3
+acr=4046
+ read -p "enter the length of the rectangle :" L
+ read -p "enter the width of the rectangle :" W
+read -p "enter the no of plots :" P
+A=$((L*W))
+Area=$((A/mtr))
+plot=$((Area*P))
+areaofplot=$((plot/acr))
+echo $areaofplot "acres"
