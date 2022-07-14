42ac62a8bab88ba12e0bd197267d6eadfd7d500f
javulm <javulmulla8@gmail.com>
2022-07-14 20:04:06 +0530

    [javul]area of rectangle

--- conversionpgm.sh
+++ conversionpgm.sh
@@ -1,9 +1,9 @@
 
-read -p "enter the number :" N
+#read -p "enter the number :" N
 
-inch=12
-feet=$((N/inch))
-echo $feet"ft"
+#inch=12
+#feet=$((N/inch))
+#echo $feet"ft"
 mtr=3
 acr=4046
  read -p "enter the length of the rectangle :" L
@@ -13,4 +13,5 @@ A=$((L*W))
 Area=$((A/mtr))
 plot=$((Area*P))
 areaofplot=$((plot/acr))
+echo $Area
 echo $areaofplot "acres"
