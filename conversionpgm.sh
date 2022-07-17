5c46412d4da936ce3c04d0b459176d1965e81d56
javulm <javulmulla8@gmail.com>
2022-07-17 18:11:57 +0530

    [javul]modified

--- conversionpgm.sh
+++ conversionpgm.sh
@@ -1,17 +1,17 @@
+#! /bin/bash
+read -p "enter the number :" N
+inch=12
+feet=$((N/inch))
+echo "$feet ft"
 
-#read -p "enter the number :" N
-
-#inch=12
-#feet=$((N/inch))
-#echo $feet"ft"
 mtr=3
 acr=4046
- read -p "enter the length of the rectangle :" L
- read -p "enter the width of the rectangle :" W
+ read -p "enter the length of the rectangle in ft:" L
+ read -p "enter the width of the rectangle in ft:" W
 read -p "enter the no of plots :" P
-A=$((L*W))
-Area=$((A/mtr))
-plot=$((Area*P))
-areaofplot=$((plot/acr))
-echo $Area
-echo $areaofplot "acres"
+A=$(( L * W ))
+Area=$(( A / mtr ))
+plot=$(( Area * P ))
+areaofplot=$(( plot / acr ))
+echo "area of the plot: $Area sqmtr"
+echo "area of the $p plots in acres:$areaofplot acres"
