f8610ac4cbb8abb25328f0cb884114f3c26d5232
javulm <javulmulla8@gmail.com>
2022-07-17 12:53:57 +0530

    [javul]modified

--- programs/randomavg.sh
+++ programs/randomavg.sh
@@ -1,12 +1,14 @@
-#! /bin/bash -x
+#!/bin/bash
 min=10
 max=99
-	num1=$((($RANDOM% $max)+$min))
+ num1=$((($RANDOM% $max)+$min))
  num2=$((($RANDOM% $max)+$min))
  num3=$((($RANDOM% $max)+$min))
  num4=$((($RANDOM% $max)+$min))
  num5=$((($RANDOM% $max)+$min))
+
 sum=$(($num1+$num2+$num3+$num4+$num5))
 avg=$(($sum/5))
-echo $avg
-
+echo "$num1+$num2+$num3+$num4+$num5"
+echo "sum: $sum"
+echo "average: $avg"
